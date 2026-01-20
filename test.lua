52ec2f59089493f66ef77e2275cc35582bcf198b
cnrpro5823 <cinarasafbulbul812@gmail.com>
2026-01-21 00:15:14 +0300

    Add ScreenGui and TextLabel for ESP Test

---
+++ test.lua
@@ -0,0 +1,5 @@
+local ScreenGui = Instance.new("ScreenGui", game.Players.LocalPlayer.PlayerGui)
+local TextLabel = Instance.new("TextLabel", ScreenGui)
+TextLabel.Size = UDim2.new(0,200,0,50)
+TextLabel.Text = "ESP Test Çalışıyor!"
+TextLabel.TextColor3 = Color3.new(1,1,1)
